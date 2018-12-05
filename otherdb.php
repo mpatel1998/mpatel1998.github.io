0e8bb3e5737d8e9a64381cea93ff29687ef82eed
odreikosen <35430980+odreikosen@users.noreply.github.com>
2018-12-05 11:41:14 -0500

    trial

--- otherdb.php
+++ otherdb.php
@@ -3,8 +3,7 @@ $conn=new mysqli("mysql1.gear.host","contest","Lo4kK51_-Y91","contest");
 if ($conn->connect_error) {
      die("Connection to database failed: " . $conn->connect_error);
 }
-$sql="UPDATE cont, SET {$_POST['name']}={$_POST['count']}";
-echo json_encode($sql);
+$sql="UPDATE cont SET {$_POST['name']}={$_POST['count']}";
 $result=$conn->query($sql);
 echo json_encode($result);
 $conn->close();
