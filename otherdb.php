59ac6ed730a20c5eb4af4e6050fd7a4d5b4d07d7
odreikosen <35430980+odreikosen@users.noreply.github.com>
2018-12-05 11:37:52 -0500

    yet another typo

--- otherdb.php
+++ otherdb.php
@@ -3,7 +3,7 @@ $conn=new mysqli("mysql1.gear.host","contest","Lo4kK51_-Y91","contest");
 if ($conn->connect_error) {
      die("Connection to database failed: " . $conn->connect_error);
 }
-$sql="UPDATE cont, SET {$_POST['name']}={_$POST['count']}";
+$sql="UPDATE cont, SET {$_POST['name']}={$_POST['count']}";
 echo json_encode($sql);
 $result=$conn->query($sql);
 echo json_encode($result);
