1f157b802e90a6449858476fcc5a4424cb23e498
odreikosen <35430980+odreikosen@users.noreply.github.com>
2018-12-05 00:04:52 -0500

    whoops

--- db.php
+++ db.php
@@ -4,8 +4,8 @@ if ($conn->connect_error) {
 
      die("Connection to database failed: " . $conn->connect_error);
 }
-$sql="select serena from cont;";
-$result=$conn->mysql_query($sql);
+$sql="select serena from cont";
+$result=$conn->query($sql);
 echo json_encode($result);
 $conn->close();
  ?>
