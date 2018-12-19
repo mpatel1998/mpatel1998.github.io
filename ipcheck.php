a0e851f1c8edbd16dab4f7db4a7de3fea769ad76
odreikosen <35430980+odreikosen@users.noreply.github.com>
2018-12-19 12:24:32 -0500

    im dumb

--- ipcheck.php
+++ ipcheck.php
@@ -8,10 +8,14 @@ $res=array();
 $sql="select 1 from chosen where ip={$_POST['ip']}";
 $result=$conn->query($sql);
 while($row=$result->fetch_assoc()){
-  array_push($res,$row)
+  array_push($res,$row);
   $condition=0;
 }
-
+if($condition==0){
+  $sql="insert into ip(ip) values ({$_POST['ip']})";
+  $result=$conn->query($sql);
+  array_push($res,0);
+}
 echo json_encode($res);
 $conn->close();
  ?>
