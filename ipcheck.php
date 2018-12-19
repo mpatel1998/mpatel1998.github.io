d86a973679de31ff0a9a5a9178e381a9dbf0973c
odreikosen <35430980+odreikosen@users.noreply.github.com>
2018-12-19 13:14:43 -0500

    hooray

--- ipcheck.php
+++ ipcheck.php
@@ -3,7 +3,7 @@ $conn=new mysqli("mysql1.gear.host","contest","Lo4kK51_-Y91","contest");
 if ($conn->connect_error) {
      die("Connection to database failed: " . $conn->connect_error);
 }
-$condition=0;
+$condition=1;
 $res=array();
 $sql="select * from ip where ip={$_POST['ip']}";
 $result=$conn->query($sql);
@@ -11,7 +11,11 @@ while($row=$result->fetch_assoc()){
   array_push($res,$row);
   $condition=0;
 }
-
+if($condition){
+  $sql="insert into ip(ip) values ({$_POST['ip']})";
+  $result=$conn->query($sql);
+  array_push($res,0);
+}
 echo json_encode($res);
 $conn->close();
  ?>
