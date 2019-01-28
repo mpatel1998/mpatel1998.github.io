4426a770fca97b7e9ba97deaef3ac829b034e04a
odreikosen <35430980+odreikosen@users.noreply.github.com>
2019-01-28 17:31:19 -0500

    hello

--- members.js
+++ members.js
@@ -1,5 +1,20 @@
+var app=angular.module("Facebook",[]);
+app.controller('FaceBookController',['$scope',function($scope){
+
+
 function checkLoginState() {
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-}
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  }
+
+FB.login(function(response){
+  if(response.status==='connected'{
+
+  } else{
+    alert('Not logged in');
+  })
+});
+
+
+}]);
