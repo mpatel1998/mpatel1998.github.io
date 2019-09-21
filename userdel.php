bda7ba466e02d133aa8cc3320f45da1bf6c6d32a
Owen <odreikosen@gmail.com>
2019-09-21 10:40:43 -0400

    legacy changes

--- userdel.php
+++ userdel.php
@@ -4,7 +4,7 @@ if ($conn->connect_error) {
      die("Connection to database failed: " . $conn->connect_error);
 }
 $res=array();
-$sql="UPDATE ip SET username = NULL WHERE ip= '{$_POST['uuid']}'";
+$sql="UPDATE ip SET username = NULL WHERE uuid= '{$_POST['uuid']}'";
 $result=$conn->query($sql);
 echo json_encode($sql);
 $conn->close();
